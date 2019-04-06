c8a494a6ce52f4b52f5390b785535ecc60537ac4
RaglandCodes <raglandasir@outlook.com>
2019-04-06 21:14:49 +0530

    added board.png

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-var app = require('express')();
+let express = require("express");
+var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+//app.use(express.static('public'));
 server.listen(5656, () => {
     console.log("Running @ port 5656");
     

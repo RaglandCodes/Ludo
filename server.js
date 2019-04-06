279231951f5f30bd4bec5ae4022bbb099e3d8567
RaglandCodes <raglandasir@outlook.com>
2019-04-07 00:26:15 +0530

    Dice rolling

--- server.js
+++ server.js
@@ -1,48 +1,69 @@
 let express = require("express");
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
 
 //app.use(express.static('public'));
 server.listen(5656, () => {
-    console.log("Running @ port 5656");
-    
+  console.log("Running @ port 5656");
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 let colourOrder = ["green", "red", "blue", "yellow"];
 let pieceData = [
-    {
-        name: "green0",
-        colour: "green",
-        locationX: 7,
-        locationY: 7,
-    }
-]
+  {
+    name: "green0",
+    colour: "green",
+    locationX: 7,
+    locationY: 7
+  }
+];
 let players = [];
-io.on('connection', function (socket) {
-  players.push({
-      id: socket.id,
-      colour:""
-  })
- console.log(players.length);
- io.to(socket.id).emit("id", {id: socket.id})
- if(players.length == 4)
- {
-     for(i=0; i<players.length; i++)
-     {
-         players[i]['colour'] = colourOrder[i];
-         
-     }
- }
+let turn = 0;
 
- io.sockets.emit("reRender", {players: players});
+io.on("connection", function(socket) {
+  players.push({
+    id: socket.id,
+    colour: ""
+  });
+  console.log(players.length);
+  io.to(socket.id).emit("id", { id: socket.id });
+  if (players.length > 3) {
+    for (i = 0; i < 4; i++) {
+      players[i]["colour"] = colourOrder[i];
+    }
+
+    io.sockets.emit("turn", {
+      turn: players[turn]["id"],
+      colour: players[turn]["colour"]
+    });
+    console.log(players[turn]["colour"]);
+  }
+
+  socket.on("diceRoll", () => {
+    console.log("dice was rolled");
+
+    if (turn == 3) turn = 0;
+    else turn += 1;
+
+    let diceNumber = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+
+    console.log(`turn = ${turn}`);
+    io.sockets.emit("turn", {
+      turn: players[turn]["id"],
+      colour: players[turn]["colour"],
+      diceState: diceNumber
+    });
+    console.log(players[turn]["colour"]);
+    io.sockets.emit("reRender", { players: players });
+  });
+  io.sockets.emit("reRender", { players: players });
   socket.on("disconnect", () => {
-    players = players.filter(player => player['id'] != socket.id);
+    players = players.filter(player => player["id"] != socket.id);
     console.log(players.length);
-    io.sockets.emit("reRender", {players: players});
+    io.sockets.emit("reRender", { players: players });
   });
 });
